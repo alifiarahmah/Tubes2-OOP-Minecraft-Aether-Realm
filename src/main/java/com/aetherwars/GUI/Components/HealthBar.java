6a2b1cf3a8f375927f18b86a11d000237c54fc0a
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2022-04-15 17:03:56 +0700

    [GUI] HealthBar updated
    Update :
    Warna -> Hijau
    Penyesuaian untuk Health Bar kiri

--- src/main/java/com/aetherwars/GUI/Components/HealthBar.java
+++ src/main/java/com/aetherwars/GUI/Components/HealthBar.java
@@ -9,15 +9,19 @@ import javax.swing.JProgressBar;
 import javax.swing.BoxLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class HealthBar extends JPanel{
     private int hp = 100;
     private String name;
     private float align = Component.LEFT_ALIGNMENT;
+    private Bar prb_health_bar;
 
-    public HealthBar(String name, float align){
+    public HealthBar(String name, int hp, float align){
         this.name = name;
         this.align = align;
+        this.hp = hp;
         render();
     }
     
@@ -26,9 +30,11 @@ public class HealthBar extends JPanel{
     
         JPanel pnl_health_bar = new JPanel();
     
-        JLabel lbl_name = new JLabel(name + " [" + hp +"]");
-        JProgressBar prb_health_bar = new JProgressBar(0,100);
-        prb_health_bar.setPreferredSize(new Dimension(250,20));
+        JLabel lbl_name = new JLabel(" " + name + " ");
+        prb_health_bar = new Bar(0,100);
+        prb_health_bar.setValue(hp);
+        prb_health_bar.setBackward(align == Component.RIGHT_ALIGNMENT);
+        prb_health_bar.setPreferredSize(new Dimension(300,20));
         pnl_health_bar.add(prb_health_bar);
         this.add(Box.createRigidArea(new Dimension(0,10)));
         
@@ -37,6 +43,30 @@ public class HealthBar extends JPanel{
         lbl_name.setAlignmentX(align);
         this.add(lbl_name);
 
-        prb_health_bar.setValue(hp);
+    }
+}
+
+class Bar extends JProgressBar{
+    private boolean isBackward = false;
+
+    public Bar(int min, int max){
+        super(min,max);
+    }
+
+    public void setBackward(boolean b){
+        isBackward = b;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if(isBackward){
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.scale(-1, 1); //Flips over y-axis
+            g2d.translate(-getWidth(), 0); //Moves back to old position.
+            
+            super.paintComponent(g2d);
+        }else{
+            super.paintComponent(g);
+        }
     }
 }
--- src/main/java/com/aetherwars/GUI/GUI.java
+++ src/main/java/com/aetherwars/GUI/GUI.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.Box;
+import javax.swing.UIManager;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
@@ -16,7 +17,8 @@ public class GUI{
     private String player1_name = "Steve";
     private String player2_name = "Alex";
     private int player1_health = 100;
-    private int player2_health = 100;
+    private int player2_health = 50;
+    private int turn = 0;
     public static void main(String args[]){
         new GUI();
     }
@@ -28,10 +30,14 @@ public class GUI{
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
         
-        HealthBar hb_player1 = new HealthBar(player1_name, Component.LEFT_ALIGNMENT);
-        HealthBar hb_player2 = new HealthBar(player2_name, Component.RIGHT_ALIGNMENT);
+        UIManager.put("ProgressBar.foreground", Color.GREEN);
+        HealthBar hb_player1 = new HealthBar(player1_name, player1_health, Component.LEFT_ALIGNMENT);
+        HealthBar hb_player2 = new HealthBar(player2_name, player2_health, Component.RIGHT_ALIGNMENT);
         
         JPanel pnl_turns = new JPanel();
+        pnl_turns.setBackground(new Color(0xCCCCCC));
+        JLabel lbl_turns = new JLabel("Turn " + turn);
+        pnl_turns.add(lbl_turns);
         JPanel pnl_north = new JPanel();
         pnl_north.setLayout(new BorderLayout());
         
