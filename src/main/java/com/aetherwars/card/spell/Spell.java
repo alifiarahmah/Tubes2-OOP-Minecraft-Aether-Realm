cb1bcbffa3f71b11cd7b5da35ee728a882a4f7c1
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-04-19 11:13:07 +0700

    [MODEL](Spell): Change use() method parameter to Character

--- src/main/java/com/aetherwars/card/spell/Spell.java
+++ src/main/java/com/aetherwars/card/spell/Spell.java
@@ -1,11 +1,12 @@
 package com.aetherwars.card.spell;
-
 import com.aetherwars.card.Card;
+import com.aetherwars.card.Character.Character;
 
 public abstract class Spell extends Card {
     public Spell(int id, String name, int mana, String description, String imagepath){
         super(id, name, mana, description, imagepath);
     }
 
-    public abstract void use(Card target);
+    public abstract void use(Character target);
+
 }
