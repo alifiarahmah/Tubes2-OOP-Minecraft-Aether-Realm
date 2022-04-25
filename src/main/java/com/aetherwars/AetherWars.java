7a223473507d4bd9260fdb5996536698ace06b3b
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-04-25 21:49:58 +0700

    connect GUI dengan main program

--- src/main/java/com/aetherwars/AetherWars.java
+++ src/main/java/com/aetherwars/AetherWars.java
@@ -5,32 +5,16 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
 import com.aetherwars.card.Character.Character;
 import com.aetherwars.card.Character.Type;
 import com.aetherwars.util.CSVReader;
 
-public class AetherWars extends Application {
+
+import com.aetherwars.view.Frame;
+//public class AetherWars extends Application {
+public class AetherWars{
   private static final String CHARACTER_CSV_FILE_PATH = "card/data/character.csv";
-
-  public void loadCards() throws IOException, URISyntaxException {
-    File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
-    CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
-    characterReader.setSkipHeader(true);
-    List<String[]> characterRows = characterReader.read();
-    for (String[] row : characterRows) {
-      Character c = new Character(Integer.parseInt(row[0]),row[1],Integer.parseInt(row[7]),
-              row[3], row[4],Type.valueOf(row[2]),Integer.parseInt(row[5]),Integer.parseInt(row[8]),
-              Float.parseFloat(row[6]),Integer.parseInt(row[9]));
-      System.out.println(c);
-    }
-  }
-
+/*
   @Override
   public void start(Stage stage) {
     Text text = new Text();
@@ -54,8 +38,27 @@ public class AetherWars extends Application {
       text.setText("Failed to load cards: " + e);
     }
   }
-
+*/
+public static void loadCards() throws IOException, URISyntaxException {
+  File characterCSVFile = new File(AetherWars.class.getResource(CHARACTER_CSV_FILE_PATH).toURI());
+  CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
+  characterReader.setSkipHeader(true);
+  List<String[]> characterRows = characterReader.read();
+  for (String[] row : characterRows) {
+    Character c = new Character(Integer.parseInt(row[0]),row[1],Integer.parseInt(row[7]),
+            row[3], row[4], Type.valueOf(row[2]),Integer.parseInt(row[5]),Integer.parseInt(row[8]),
+            Float.parseFloat(row[6]),Integer.parseInt(row[9]));
+    System.out.println(c);
+  }
+}
   public static void main(String[] args) {
-    launch();
+    //launch();
+    Frame main_frame = new Frame();
+    try {
+      AetherWars.loadCards();
+    } catch (Exception e) {
+      System.out.println("Failed to load cards: " + e);
+    }
+    main_frame.run();
   }
 }
