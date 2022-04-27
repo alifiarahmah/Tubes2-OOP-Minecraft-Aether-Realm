7a260af4a8393451568c3cc82ca8bc11846abe8d
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-04-27 23:36:26 +0700

    [MODEL](Level) Add mana check in LevelSpell use()

--- src/main/java/com/aetherwars/card/spell/LevelSpell.java
+++ src/main/java/com/aetherwars/card/spell/LevelSpell.java
@@ -24,10 +24,15 @@ public class LevelSpell extends Spell {
         Meningkatkan/menurunkan level dari sebuah karakter sebanyak 1 serta mereset exp (membuat exp menjadi 0).
         Batas minimal level tetap 1 dan maksimal tetap 10. */
 
-        target.setLevel(this.getLevelChange() > 0
-                ? Math.min(target.getLevel() + this.levelChange, 10)
-                : Math.max(target.getLevel() + this.levelChange, 1)
-        );
-        target.setExp(0);
+        if (target.getMana() >= this.getMana()) {
+            target.setLevel(this.getLevelChange() > 0
+                    ? Math.min(target.getLevel() + this.levelChange, 10)
+                    : Math.max(target.getLevel() + this.levelChange, 1)
+            );
+            target.setMana(target.getMana() - this.getMana());
+            target.setExp(0);
+        } else {
+            System.out.println("Not enough mana!");
+        }
     }
 }
