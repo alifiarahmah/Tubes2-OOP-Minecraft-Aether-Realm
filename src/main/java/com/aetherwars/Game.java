2b56064490db4856017b90c4c7bca129a0cfbd3a
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2022-04-27 20:23:22 +0700

    [MAIN] add pubsub to Game.java

--- src/main/java/com/aetherwars/Game.java
+++ src/main/java/com/aetherwars/Game.java
@@ -2,7 +2,7 @@ package com.aetherwars;
 import com.aetherwars.model.*;
 import com.aetherwars.event.*;
 
-public class Game {
+public class Game implements Publisher, Subscriber{
     private Player players[];
     private int turns;
     private Phase phase;
@@ -15,5 +15,29 @@ public class Game {
         this.players[1] = p2;
         this.channel = channel;
         this.cur_player = 0;
+    }
+    
+    public void setup(){
+        // di sini harusnya draw 3 kartu dulu
+        // terus tiap player reset health
+        // publish event playerchanged buat ngubah giliran player jadi giliran p1
+        // terus pake stageController buat masuk fase draw
+    }
+    
+    public void stageController(Phase phase){
+        if(phase == Phase.DRAW){
+            // draw
+        }else if (phase == Phase.END){
+            // end phase
+        }
+    }
+    @Override
+    public void onEvent(Event event){
+        // kalo eventnya PhaseChangeEvent
+    }
+    
+    @Override
+    public void publish(Event event){
+        this.channel.sendEvent(this, event);
     }
 }
