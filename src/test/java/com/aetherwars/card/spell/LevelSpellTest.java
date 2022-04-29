20a23a7b29f1ea1ef5f12b74ee8f1d18945d1415
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-04-29 15:49:24 +0700

    [TEST] Modified LevelSpellTest to use @Before

--- src/test/java/com/aetherwars/card/spell/LevelSpellTest.java
+++ src/test/java/com/aetherwars/card/spell/LevelSpellTest.java
@@ -3,63 +3,59 @@ package com.aetherwars.card.spell;
 import com.aetherwars.card.Character.Character;
 import com.aetherwars.card.Character.Type;
 import com.aetherwars.card.SummonedCharacter;
-import org.junit.Assert;
+import org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class LevelSpellTest {
+      private LevelSpell spell;
+      private Character character;
+      private SummonedCharacter summonedCharacter;
+
+    @Before
+    public void setUp() {
+        spell = new LevelSpell(1, "LvlUp1", 4, "", "", 1);
+        character = new Character(1, "", 5, "", "",
+                Type.OVERWORLD, 10, 2, 3, 5);
+        summonedCharacter = new SummonedCharacter(character, 1);
+    }
 
     @Test
     public void testLevelUp() {
-        Spell spell = new LevelSpell(1, "LvlUp1", 4, "", "", 1);
-        Character character = new Character(1, "", 5, "", "",
-                Type.OVERWORLD, 10, 2, 3, 5);
-        SummonedCharacter summonedCharacter = new SummonedCharacter(character, 1);
         spell.use(summonedCharacter);
-        Assert.assertEquals(2, summonedCharacter.getLevel());
-        Assert.assertEquals(1, character.getMana());
+        assertEquals(2, summonedCharacter.getLevel());
+        assertEquals(1, character.getMana());
     }
 
     @Test
     public void testLevelDown() {
-        Spell spell = new LevelSpell(1, "LvlDown1", 3, "", "", -1);
-        Character character = new Character(1, "", 5, "", "",
-                Type.OVERWORLD, 10, 2, 3, 5);
-        SummonedCharacter summonedCharacter = new SummonedCharacter(character, 1);
+        spell.setLevelChange(-1);
         spell.use(summonedCharacter);
-        Assert.assertEquals(1, summonedCharacter.getLevel());
-        Assert.assertEquals(2, character.getMana());
+        assertEquals(1, summonedCharacter.getLevel());
+        assertEquals(1, character.getMana());
     }
 
     @Test
     public void testMaxLevelUp() {
-        Spell spell = new LevelSpell(1, "LvlUp", 4, "", "", 1);
-        Character character = new Character(1, "", 5, "", "",
-                Type.OVERWORLD, 10, 2, 3, 5);
-        SummonedCharacter summonedCharacter = new SummonedCharacter(character, 1);
         summonedCharacter.setLevel(10);
-        spell.use(summonedCharacter);
-        spell.use(summonedCharacter);
-        Assert.assertEquals(10, summonedCharacter.getLevel());
+        this.spell.use(summonedCharacter);
+        assertEquals(10, summonedCharacter.getLevel());
     }
 
     @Test
     public void testMaxLevelDown() {
-        Spell spell = new LevelSpell(1, "LvlUp", 4, "", "", -1);
-        Character character = new Character(1, "", 5, "", "",
-                Type.OVERWORLD, 10, 2, 3, 5);
-        SummonedCharacter summonedCharacter = new SummonedCharacter(character, 1);
+        spell.setLevelChange(-1);
         spell.use(summonedCharacter);
-        Assert.assertEquals(1, summonedCharacter.getLevel());
+        assertEquals(1, summonedCharacter.getLevel());
     }
 
     @Test
     public void testInsufficentMana() {
-        Spell spell = new LevelSpell(1, "LvlUp", 999, "", "", 2);
-        Character character = new Character(1, "", 5, "", "",
-                Type.OVERWORLD, 10, 2, 3, 5);
-        SummonedCharacter summonedCharacter = new SummonedCharacter(character, 1);
+        spell.setMana(999);
         spell.use(summonedCharacter);
-        Assert.assertEquals(1, summonedCharacter.getLevel());
+        assertEquals(1, summonedCharacter.getLevel());
     }
 
 }
